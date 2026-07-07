72d365fc8c96ab341957b5a7872bbe587dc862ae
soy1040 <soy10401@gmail.com>
2026-07-07 18:26:56 +0900

    first commit

---
+++ src/main/java/com/example/rootin/RootinApplication.java
@@ -0,0 +1,13 @@
+package com.example.rootin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootinApplication.class, args);
+    }
+
+}
