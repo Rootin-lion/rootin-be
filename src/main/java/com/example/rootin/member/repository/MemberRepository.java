c6aa68bf5068b53788ee9e99e9eb729d53aaf872
soy1040 <soy10401@gmail.com>
2026-07-14 15:55:16 +0900

    Member, MemberRepository

---
+++ src/main/java/com/example/rootin/member/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.example.rootin.member.repository;
+
+import com.example.rootin.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository
+        extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByProviderAndProviderId(
+            String provider,
+            String providerId
+    );
+
+    boolean existsByNickname(String nickname);
+}
